48d35d8cf509aef9efd6997fd9be2c4f12c5b6ff
Kirill Tkachenko <kirill.tkachenko7@gmail.com>
2020-02-09 21:20:14 +0300

    add form for creating a new post

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Group
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from django.contrib.auth import get_user_model
 
 
 def index(request):
@@ -20,4 +23,25 @@ def group_posts(request, slug):
     # get 12 latest posts in the group
     latest = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, 'group.html', {'group': group, 'posts': latest})
+
+
+@login_required
+def new_post(request):
+    """display a form for adding a new post to authenticated users"""
+    if request.method == 'POST':
+        # if we got a POST request, validate form
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # if form is valid, populate missing data and save a post
+            # all validation is done at the model level
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            # redirect user to the home page
+            return redirect('index')
+        # if form is not valid, display the same form and show validation errors
+        return render(request, 'new_post.html', {'form': form})
+    # if this is not a POST request, display a blank PostForm
+    form = PostForm()
+    return render(request, 'new_post.html', {'form': form})
 
