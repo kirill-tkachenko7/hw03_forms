39c0f8f7485d9d7e402cae2ce91f65da0a2445f4
= <=>
2020-02-28 00:41:57 +0300

    added paginator to groups

--- posts/views.py
+++ posts/views.py
@@ -8,24 +8,28 @@ from .models import Post, Group
 
 
 def index(request):
+    """ display latest posts """
     post_list = Post.objects.order_by("-pub_date").all()
-    paginator = Paginator(post_list, 10) # показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10) # display 10 posts per page
 
-    page_number = request.GET.get('page') # переменная в URL с номером запрошенной страницы
-    page = paginator.get_page(page_number) # получить записи с нужным смещением
+    page_number = request.GET.get('page') 
+    page = paginator.get_page(page_number) # retreive posts with correct offset
     return render(request, 'index.html', {'page': page, 'paginator': paginator})
 
 
 def group_posts(request, slug):
-    """ show 12 latest posts in the group """
+    """ display latest posts in the group """
 
     # get group object using a slug passed in the URL
-    # pylint: disable=no-member
     group = get_object_or_404(Group, slug=slug)
 
-    # get 12 latest posts in the group
-    latest = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    return render(request, 'group.html', {'group': group, 'posts': latest})
+    # show 10 posts per page
+    post_list = Post.objects.filter(group=group).order_by("-pub_date").all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    return render(request, 'group.html', {'group': group, 'page': page, 'paginator': paginator})
 
 
 @login_required
